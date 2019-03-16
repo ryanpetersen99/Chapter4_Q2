61b008a42957db93ed1cb137218b287acad1d19d
ryanpetersen99 <47520518+ryanpetersen99@users.noreply.github.com>
2019-03-16 22:15:51 +0200

    Update

--- src/main/java/za/ac/cput/Honda.java
+++ src/main/java/za/ac/cput/Honda.java
@@ -1,4 +1,28 @@
 package za.ac.cput;
 
-public class Honda {
+public class Honda{
+    String model;
+    Car c;
+
+    public Honda(){
+        this.model = "Honda";
+        c = new Car();
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setMethod(int gears,int speed,String model){
+        c.setSpeedGears(speed,gears);
+        this.model = model;
+    }
+
+    public String toString(){
+        return ("The car model is " + getModel() + "\n" +  c.toString());
+    }
+
+    public void runApp(){
+        System.out.print("The Details Of The Car Are: " + "\n" + toString());
+    }
 }
